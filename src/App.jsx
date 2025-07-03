9f69918832cea5eea995166db7359aeed907ae22
barbaracortez <cortezbarbara.dev@gmail.com>
2025-07-03 11:57:03 -0300

    agregar carrito y eliminar productos

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,41 @@ import { useState } from "react";
 import products from "./data/products";
 import Header from "./components/Header";
 import ProductCard from "./components/ProductCard";
+import SearchBar from "./components/SearchBar";
+import Cart from "./components/Cart";
 import "./App.css";
 
 function App() {
   const [cart, setCart] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   const addToCart = (product) => {
     setCart([...cart, product]);
   };
 
+  const filtereProducts = products.filter((products)=>
+  products.name.toLowerCase().includes(searchTerm.toLowerCase())
+);
+
   return (
     <div className="app">
       <Header cartCount={cart.length} />
-      <div className="product-grid">
-        {products.map((product) => (
+      <div className="top-bar">
+        <SearchBar value={searchTerm} onChange={setSearchTerm} />
+      </div>
+
+      <div className = "main-content">
+        <div className="product-grid">
+        {filtereProducts.map((product) => (
           <ProductCard
             key={product.id}
             product={product}
             onAddToCart={addToCart}
           />
         ))}
+        </div>
+
+      <Cart cart={cart} />
       </div>
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import "./Header.css"
 function Header ({cartCount}) {
     return (
         <header className = "header">
-            <h1 className ="title">Catálogo de Rpoductos</h1>
+            <h1 className ="title">Catálogo de Productos</h1>
             <span className="cart-count">Carrito: {cartCount}</span>
         </header>
     );
